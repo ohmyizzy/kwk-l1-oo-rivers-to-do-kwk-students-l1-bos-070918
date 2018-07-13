4cc86e771d0603be68c4d30a867a0f6436667c4b
ohmyizzy <conneri19@barringtonschools.org>
2018-07-13 15:02:24 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,16 @@
 # river.rb
+
+class River 
+  attr_accessor :name, :length, :countries, :discharge
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def flood(discharge)
+    @discharge = discharge * 1.3
+  end
+  
+  def dry_up(discharge)
+    @discharge = discharge * .5
+  end
